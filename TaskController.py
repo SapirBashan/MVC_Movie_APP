58592abd61d09ea0446dcf3ab7a3f3f119a7bd4b
noam.benisho@gmail.com <noam.benisho@gmail.com>
2024-03-20 11:48:46 +0200

    SOC fix

--- TaskController.py
+++ TaskController.py
@@ -1,3 +1,44 @@
+# # TaskController.py
+
+# from PyQt6.QtCore import Qt
+# import logging
+
+# class TodoController:
+#     def __init__(self, model, view):
+#         self.model = model
+#         self.view = view
+#         self.view.setController(self)
+#         logging.basicConfig(level=logging.INFO)
+
+#     def addTodo(self, text):
+#         self.model.insertRows(len(self.model.todos), 1)
+#         self.model.todos[-1] = (False, text)
+#         self.model.dataChanged.emit(self.model.index(len(self.model.todos) - 1), self.model.index(len(self.model.todos) - 1))
+#         self.printTodos()
+
+#     def deleteTodo(self, index):
+#         todo = self.model.todos[index.row()]
+#         self.model.removeRows(index.row(), 1)
+     
+
+#     def printTodos(self):
+#         for i, todo in enumerate(self.model.todos):
+#             status, text = todo
+#             status_str = 'Done' if status else 'Not done'
+
+#     def completeTodo(self, index):
+#         row = index.row()
+#         _, text = self.model.todos[row]
+#         self.model.todos[row] = (True, text)
+#         # Emit signal to notify view about the data change
+#         self.model.dataChanged.emit(index, index)
+#         self.printTodos()
+
+#     def updateView(self):
+#         # Set the model for the QListView
+#         self.view.todoList.setModel(self.model)
+
+
 # TaskController.py
 
 from PyQt6.QtCore import Qt
@@ -19,7 +60,6 @@ class TodoController:
     def deleteTodo(self, index):
         todo = self.model.todos[index.row()]
         self.model.removeRows(index.row(), 1)
-     
 
     def printTodos(self):
         for i, todo in enumerate(self.model.todos):
@@ -37,3 +77,6 @@ class TodoController:
     def updateView(self):
         # Set the model for the QListView
         self.view.todoList.setModel(self.model)
+        
+    def searchMovieController(self, movie_name):
+        return self.model.fetchMovieData(movie_name)
--- TaskModel.py
+++ TaskModel.py
@@ -1,10 +1,47 @@
+# # TaskModel.py
+# from PyQt6.QtCore import Qt, QModelIndex
+# from PyQt6.QtGui import QStandardItemModel, QStandardItem
+# from PyQt6.QtCore import QAbstractListModel
+# import requests
+
+
+
+# class TodoModel(QAbstractListModel):
+#     def __init__(self, todos=None):
+#         super().__init__()
+#         self.todos = todos or []
+
+#     def data(self, index, role):
+#         if role == Qt.ItemDataRole.DisplayRole:
+#             _, text = self.todos[index.row()]
+#             return text
+#         if role == Qt.ItemDataRole.CheckStateRole:
+#             status, _ = self.todos[index.row()]
+#             return Qt.CheckState.Checked if status else Qt.CheckState.Unchecked
+
+#     def rowCount(self, index):
+#         return len(self.todos)
+
+#     def insertRows(self, position, rows, index = QModelIndex()):
+#         self.beginInsertRows(index, position, position + rows - 1)
+#         for _ in range(rows):
+#             self.todos.insert(position, (False, ""))
+#         self.endInsertRows()
+#         return True
+
+#     def removeRows(self, position, rows, index = QModelIndex()):
+#         self.beginRemoveRows(index, position, position + rows - 1)
+#         for _ in range(rows):
+#             del self.todos[position]
+#         self.endRemoveRows()
+#         return True
+
+
 # TaskModel.py
 from PyQt6.QtCore import Qt, QModelIndex
 from PyQt6.QtGui import QStandardItemModel, QStandardItem
 from PyQt6.QtCore import QAbstractListModel
 import requests
-
-
 
 class TodoModel(QAbstractListModel):
     def __init__(self, todos=None):
@@ -22,16 +59,26 @@ class TodoModel(QAbstractListModel):
     def rowCount(self, index):
         return len(self.todos)
 
-    def insertRows(self, position, rows, index = QModelIndex()):
+    def insertRows(self, position, rows, index=QModelIndex()):
         self.beginInsertRows(index, position, position + rows - 1)
         for _ in range(rows):
             self.todos.insert(position, (False, ""))
         self.endInsertRows()
         return True
 
-    def removeRows(self, position, rows, index = QModelIndex()):
+    def removeRows(self, position, rows, index=QModelIndex()):
         self.beginRemoveRows(index, position, position + rows - 1)
         for _ in range(rows):
             del self.todos[position]
         self.endRemoveRows()
         return True
+
+    def fetchMovieData(self, movie_name):
+        movie_response = requests.get('https://localhost:7276/Movie/' + movie_name, headers={'accept': 'application/json'}, verify=False)
+        print(f"Response status code: {movie_response.status_code}")  # Print the status code
+
+        if movie_response.status_code == 200:  # Check if the request was successful
+            return movie_response.json()
+        else:
+            print(f"Error: {movie_response.text}")  # Print the error message
+            return None
