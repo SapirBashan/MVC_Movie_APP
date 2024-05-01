fb3fe35be64ff6c1568e7db683940a0e92b67bd1
noam.benisho@gmail.com <noam.benisho@gmail.com>
2024-05-01 15:54:05 +0300

    double click write

--- TaskView.py
+++ TaskView.py
@@ -1,9 +1,8 @@
 import requests
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QLineEdit, QListView, QWidget, QListWidget, QLabel, QListWidgetItem, QGridLayout
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPixmap, QIcon
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QLineEdit, QLabel, QWidget, QGridLayout
+from PyQt6.QtGui import QPixmap
 import urllib3
-#from qt_material import apply_stylesheet
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -19,20 +18,57 @@ class TodoView(QMainWindow):
         self.removeButton = QPushButton("Remove", self)
         self.searchButton = QPushButton("Search", self)
         self.movieLabel = QLabel(self)
-        self.posterLayout = QGridLayout()  # Define posterLayout as a QGridLayout
+        self.posterLayout = QGridLayout()  
 
         self.layout.addWidget(self.movieEdit)
         self.layout.addWidget(self.addButton)
         self.layout.addWidget(self.removeButton)
         self.layout.addWidget(self.searchButton)
         self.layout.addWidget(self.movieLabel)
-        self.layout.addLayout(self.posterLayout)  # Add posterLayout to the main layout
+        self.layout.addLayout(self.posterLayout)  
 
-        # Apply a theme to the application
-        #apply_stylesheet(self.app, theme='dark_blue.xml')
-        # Set controller
         self.controller = None
 
+    def updateMovieUI(self, movie_data, flag):
+        movie_details = ""
+        if movie_data['title'] != "N/A" and movie_data['title'] is not None:
+            movie_details += f"Title: {movie_data['title']}\n"
+        if movie_data['year'] != "N/A" and movie_data['year'] is not None:
+            movie_details += f"Year: {movie_data['year']}\n"
+        if movie_data['rated'] != "N/A" and movie_data['rated'] is not None:
+            if movie_data['rated'] == "R":
+                movie_details += f"Rated: SHIGETZ\n"
+            elif movie_data['rated'] == "PG-13" or movie_data['rated'] == "PG":
+                movie_details += f"Rated: BE-DI-AVAD\n"
+            elif movie_data['rated'] == "G":
+                movie_details += f"Rated: KOSHER\n"
+            else:
+                movie_details += f"Rated: {movie_data['rated']}\n"
+        if movie_data['type'] != "N/A" and movie_data['type'] is not None:
+            movie_details += f"Type: {movie_data['type']}\n"
+        if movie_data['plot'] != "N/A" and movie_data['plot'] is not None:
+            movie_details += f"Plot: {movie_data['plot']}\n"
+        if movie_data['imdbRating'] != "N/A" and movie_data['imdbRating'] is not None:
+            movie_details += f"IMDB Rating: {movie_data['imdbRating']}\n"
+
+        if movie_details == "":
+            movie_details = "No movie data"
+        
+        self.movieLabel.setWordWrap(True)
+        self.movieLabel.setText(movie_details)
+
+        if movie_data['poster'] != 'N/A' and movie_data['poster'] is not None:
+            if flag:
+                layout = QVBoxLayout(self.central_widget)
+                text_label = QLabel("Hello, PyQt!")
+                layout.addWidget(text_label)
+            else:
+                data = requests.get(movie_data['poster']).content
+                pixmap = QPixmap()
+                pixmap.loadFromData(data)
+                self.movieLabel.setPixmap(pixmap)
+        else:
+            self.movieLabel.clear()
 
     def updateMovieList(self, movie_posters):
         # Clear existing posters
@@ -89,53 +125,20 @@ class TodoView(QMainWindow):
         movie_name = self.movieEdit.text()
         movie_data = self.controller.searchMovieController(movie_name)
         if movie_data:
-            self.updateMovieUI(movie_data)
+            self.updateMovieUI(movie_data, True)
             self.updateMovieList([movie_data['poster']])  # Update poster layout with the poster URL
         else:
             self.movieLabel.setText(f"Failed to fetch movie data for {movie_name}")
-
-    def updateMovieUI(self, movie_data):
-        movie_details = ""
-        if movie_data['title'] != "N/A" and movie_data['title'] is not None:
-            movie_details += f"Title: {movie_data['title']}\n"
-        if movie_data['year'] != "N/A" and movie_data['year'] is not None:
-            movie_details += f"Year: {movie_data['year']}\n"
-        if movie_data['rated'] != "N/A" and movie_data['rated'] is not None:
-            if movie_data['rated'] == "R":
-                movie_details += f"Rated: SHIGETZ\n"
-            elif movie_data['rated'] == "PG-13" or movie_data['rated'] == "PG":
-                movie_details += f"Rated: BE-DI-AVAD\n"
-            elif movie_data['rated'] == "G":
-                movie_details += f"Rated: KOSHER\n"
-            else:
-                movie_details += f"Rated: {movie_data['rated']}\n"
-        if movie_data['type'] != "N/A" and movie_data['type'] is not None:
-            movie_details += f"Type: {movie_data['type']}\n"
-        if movie_data['plot'] != "N/A" and movie_data['plot'] is not None:
-            movie_details += f"Plot: {movie_data['plot']}\n"
-        if movie_data['imdbRating'] != "N/A" and movie_data['imdbRating'] is not None:
-            movie_details += f"IMDB Rating: {movie_data['imdbRating']}\n"
-
-        if movie_details == "":
-            movie_details = "No movie data"
-        
-        self.movieLabel.setWordWrap(True)
-        self.movieLabel.setText(movie_details)
-
-        if movie_data['poster'] != 'N/A' and movie_data['poster'] is not None:
-            data = requests.get(movie_data['poster']).content
-            pixmap = QPixmap()
-            pixmap.loadFromData(data)
-            self.movieLabel.setPixmap(pixmap)
-        else:
-            self.movieLabel.clear()
 
     def showMovieDetails(self, url):
         movie_data = self.controller.getMovieDetailsByPosterController(url)
         if isinstance(movie_data, list):
             for movie in movie_data:
-                self.updateMovieUI(movie)
+                if isinstance(movie, dict):
+                    self.updateMovieUI(movie, True)
+                else:
+                    print("Invalid movie data format.")
         elif isinstance(movie_data, dict):
-            self.updateMovieUI(movie_data)
+            self.updateMovieUI(movie_data, True)
         else:
             print(f"Unexpected type {type(movie_data)} for movie_data")
